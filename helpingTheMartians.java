782e24efc13edbd5448b204e7a853effeae0ce2d
bfwuw-rsrkf <zy.130606@gmail.com>
2025-01-30 13:13:10 +0600

    feat(code): introduce main game mechanic
    Player takes 3 guesses one by one, after which the game says, how many of the guesses were correct. After 5 attempts of not finding all the cargos at once, the cargos run to their new positions. Game will end, when all of the cargos will be found simultaneously at the same attempt
    
    BREAKING CHANGE: use while loop to simulate game main body. ArrayList correctGuesses is used to ensure unique correct guesses. Number of correctGuesses is updated after each attempt to avoid accumulation of correctGuesses from attempt to attempt, which would lead to wrong results

--- helpingTheMartians.java
+++ helpingTheMartians.java
@@ -1,8 +1,46 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.Random;
 
 class helpingTheMartians {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] cargoMarks = cargoScatter(null);
+        ArrayList<Integer> correctGuesses = new ArrayList<>();
+        int incorrectGuesses = 0;
 
+        while (true) {
+            for (int i = 0; i < 3; i++) {
+                System.out.print("Take your guess, where a cargo might be: ");
+                int guess = input.nextInt();
+
+                if (contains(cargoMarks, guess) && !(correctGuesses.contains(guess))) {
+                    correctGuesses.add(guess);
+                }
+            }
+
+            if (correctGuesses.size() < 3) {
+                System.out.printf("You have found %d cargos out of 3!\n", correctGuesses.size());
+                correctGuesses.clear();
+                incorrectGuesses++;
+            }
+            else {
+                break;
+            }
+
+            if (incorrectGuesses == 5) {
+                System.out.println("Cargos found out that you're trying to find them! They're running away...");
+                cargoMarks = cargoScatter(cargoMarks);
+                incorrectGuesses = 0;
+            }
+        }
+
+        System.out.println("You have found all of the cargos! Congratulations!\n" +
+            "Their positions are:");
+        for (int i = 0; i < 3; i++) {
+            System.out.println(correctGuesses.get(i));
+        }
+        input.close();
     }
 
     static Boolean contains(int[] arr, int elem) {
